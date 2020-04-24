9475fe847163c411319f832c8be40e00262d82d0
riolly <albert.r.cahyantara@gmail.com>
2020-04-24 08:07:57 +0700

    Config heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const session = require('express-session')
-const port = 3000
+const port = process.env.PORT || 4000
 const route = require('./routes/index.js')
 
 
